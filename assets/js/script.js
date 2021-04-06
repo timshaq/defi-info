e10a291e4964b1cd758628ffcc5155d68cbb1922
testtimshaq <58642417+testtimshaq@users.noreply.github.com>
2021-04-06 10:00:35 +0500

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import webpTest from './webpTest';
+
+webpTest();
+
+document.addEventListener("DOMContentLoaded", function () {
+
+const header = document.getElementById('header');
+const html = document.getElementById('html');
+
+
+});
